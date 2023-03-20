cd04a93f141f477de35b301210a23a1fd1d61094
rhit-davisoc <78520741+rhit-davisoc@users.noreply.github.com>
2023-03-20 11:18:55 -0400

    Labelled Delayed SIR model

--- SIR.m
+++ SIR.m
@@ -1,20 +1,31 @@
 function [] = SIR()
 
-lag = 10;
+lag = [10];
 history = [30*10^6; 30; 38];
 
-sol1 = ddesd(@(t,P,Pdel) calcDP(t,P,Pdel,0.01),lag,history,[0,180]);
+sol1 = dde23(@(t,P,Pdel) calcDP(t,P,Pdel,0.01),lag,history,[0,180]);
 
 plot(sol1.x,sol1.y(1,:),'--g',sol1.x,sol1.y(3,:),'--r');
 
-sol2 = ddesd(@(t,P,Pdel) calcDP(t,P,Pdel,0.02),lag,history,[0,180]);
+sol2 = dde23(@(t,P,Pdel) calcDP(t,P,Pdel,0.02),lag,history,[0,180]);
 
 hold on
 plot(sol2.x,sol2.y(1,:),'g',sol2.x,sol2.y(3,:),'r');
+
+title("Delayed SIR Model (Susceptible/Recovered)");
+ylabel("Population");
+xlabel("Time (t)");
+legend("Susceptible, u=0.01","Recovered, u=0.01","Susceptible, u=0.02","Recovered, u=0.02");
+
 hold off
 
 figure;
 plot(sol1.x,sol1.y(2,:),'--k',sol2.x,sol2.y(2,:),'k');
+
+title("Delayed SIR Model (Infected)");
+ylabel("Population");
+xlabel("Time (t)");
+legend("u=0.01","u=0.02");
 
 end
 
