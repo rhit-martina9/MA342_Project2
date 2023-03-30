3f07e45ecca160c97bcb950b8228f5420865ad19
rhit-martina9 <89958644+rhit-martina9@users.noreply.github.com>
2023-03-30 09:47:54 -0400

    SIR Progress + Slideshow

--- SIR.m
+++ SIR.m
@@ -1,14 +1,39 @@
 function [] = SIR()
 close all
 lag = [10];
-history = [2*10^(7); 30; 28];
+history = [3*10^(7); 30; 28];
 options = odeset('NormControl', 'on', 'MaxStep', 1);
+endStep = 200;
 
-sol11 = dde23(@(t,P,Pdel) calcDP(t,P,Pdel,0.01,lag,0),lag,history,[0,180],options);
-sol12 = dde23(@(t,P,Pdel) calcDP(t,P,Pdel,0.01,lag,1),lag,history,[0,180],options);
-sol21 = dde23(@(t,P,Pdel) calcDP(t,P,Pdel,0.02,lag,0),lag,history,[0,180],options);
-sol22 = dde23(@(t,P,Pdel) calcDP(t,P,Pdel,0.02,lag,1),lag,history,[0,180],options);
+sol10 = dde23(@(t,P,Pdel) calcDP(t,P,Pdel,  [],0),lag,history,[0,endStep],options);
+%sol11 = dde23(@(t,P,Pdel) calcDP(t,P,Pdel,0.01,1),lag,history,[0,endStep],options);
+%sol12 = dde23(@(t,P,Pdel) calcDP(t,P,Pdel,  [],2),lag,history,[0,endStep],options);
+%sol13 = dde23(@(t,P,Pdel) calcDP(t,P,Pdel,0.01,3),lag,history,[0,endStep],options);
+%sol21 = dde23(@(t,P,Pdel) calcDP(t,P,Pdel,0.02,1),lag,history,[0,endStep],options);
+%sol23 = dde23(@(t,P,Pdel) calcDP(t,P,Pdel,0.02,3),lag,history,[0,endStep],options);
 
+% basic figure
+hold on
+plot(sol10.x,sol10.y(1,:),'g',sol10.x,sol10.y(3,:),'r');
+
+title("SIR Model (Susceptible/Recovered)");
+ylabel("Population");
+xlabel("Time (t)");
+legend("Susceptible","Recovered");
+hold off
+
+figure;
+hold on
+plot(sol10.x,sol10.y(2,:),'k');
+
+title("SIR Model (Infected)");
+ylabel("Population");
+xlabel("Time (t)");
+legend("Susceptible","Recovered");
+hold off
+
+% with vaccinations/short-lived disease
+figure;
 hold on
 plot(sol11.x,sol11.y(1,:),'--g',sol11.x,sol11.y(3,:),'--r');
 plot(sol21.x,sol21.y(1,:),  'g',sol21.x,sol21.y(3,:),  'r');
@@ -40,7 +65,7 @@ plot(sol22.x,sol22.y(1,:)-sol21.y(1,:),  'g',sol22.x,sol22.y(3,:)-sol21.y(3,:), 
 title("Delayed SIR Model (Susceptible/Recovered) - Delay Difference");
 ylabel("Population");
 xlabel("Time (t)");
-legend("Susceptible, u=0.01","Recovered, u=0.01","Susceptible, u=0.02","Recovered, u=0.02", "Susceptible, u=0.01","Recovered, u=0.01","Susceptible, u=0.02","Recovered, u=0.02");
+legend("Susceptible, u=0.01","Recovered, u=0.01","Susceptible, u=0.02","Recovered, u=0.02");
 
 hold off
 
@@ -58,6 +83,23 @@ figure;
 plot(sol12.x,sol12.y(2,:),'--k',sol22.x,sol22.y(2,:),'k');
 
 title("Delayed SIR Model (Infected) - with Delay");
+ylabel("Population");
+xlabel("Time (t)");
+legend("u=0.01","u=0.02");
+
+figure;
+plot(sol11.x,sol11.y(1,:)+sol11.y(2,:)+sol11.y(3,:),'--k',sol21.x,sol21.y(1,:)+sol21.y(2,:)+sol21.y(3,:),'k');
+
+title("Delayed SIR Model (Total Population)");
+ylabel("Population");
+xlabel("Time (t)");
+legend("u=0.01","u=0.02");
+
+
+figure;
+plot(sol12.x,sol12.y(1,:)+sol12.y(2,:)+sol12.y(3,:),'--k',sol22.x,sol22.y(1,:)+sol22.y(2,:)+sol22.y(3,:),'k');
+
+title("Delayed SIR Model (Total Population) - with Delay");
 ylabel("Population");
 xlabel("Time (t)");
 legend("u=0.01","u=0.02");
--- calcDP.m
+++ calcDP.m
@@ -1,17 +1,36 @@
-function [dP] = calcDP(t, P, Pdel, u, lag, delay)
-    Gamma = 1174.17;
-    beta = 0.3095;
-    d = 3.9139*10^(-5);
+function [dP] = calcDP(t, P, Pdel, u, selection)
+    rho = 1174.17;
+    alpha = 0.3095;
+    delta = 3.9139*10^(-5);
     eps = 0.0063;
-    gamma = 0.2;
+    beta = 0.2;
+    
     dP = zeros(3,1);
     N = P(1)+P(2)+P(3);
-    dP(1) = Gamma - beta*P(1)*P(2)/N - u*Pdel(1,1);
-    dP(2) = beta*P(1)*P(2)/N - (gamma+d+eps)*P(2);
-    dP(3) = gamma*P(2) - d*P(3) + u*Pdel(1,1);
-    if t>=lag && delay == 1
+    if selection == 0 % Basic Model
+        dP(1)=-alpha*P(1)*P(2);
+        dP(2)=alpha*P(1)*P(2)-beta*P(2);
+        dP(3)=beta*P(2);
+    elseif selection == 1 % Vaccinations, no infected delay
+        dP(1)=rho-alpha*P(1)*P(2)/N - delta*S(t)-u*Pdel(1,1);
+        dP(2)=alpha*P(1)*P(2)/N-(beta+delta+eps)*P(2);
+        dP(3)=beta*P(2)-delta*P(2)+u*Pdel(1,1);
+    elseif selection == 2 % Infected delay, no vaccinations
+        dP(1)=rho-alpha*P(1)*P(2)/N - delta*S(t);
+        dP(2)=alpha*P(1)*P(2)/N-(beta+delta+eps)*P(2);
+        dP(3)=beta*P(2)-delta*P(2);
+        
+        old_dI = beta*Pdel(1,1)*Pdel(2,1)/N - (gamma+d+eps)*Pdel(2,1);
+        dP(2) = dP(2) - old_dI;
+        dP(3) = dP(3) + old_dI;
+    elseif selection == 3 % Infected delay and vaccinations
+        dP(1)=rho-alpha*P(1)*P(2)/N - delta*S(t)-u*Pdel(1,1);
+        dP(2)=alpha*P(1)*P(2)/N-(beta+delta+eps)*P(2);
+        dP(3)=beta*P(2)-delta*P(2)+u*Pdel(1,1);
+        
         old_dI = beta*Pdel(1,1)*Pdel(2,1)/N - (gamma+d+eps)*Pdel(2,1);
         dP(2) = dP(2) - old_dI;
         dP(3) = dP(3) + old_dI;
     end
+    
 end
